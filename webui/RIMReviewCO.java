ce1be30a3320a7a3126ac1ff6824931574b00cc7
rei-gasis <rei.gasis@gmail.com>
2021-06-21 22:53:51 +0800

    *Fixed Closing Workflow

--- webui/RIMReviewCO.java
+++ webui/RIMReviewCO.java
@@ -369,13 +369,14 @@ public class RIMReviewCO extends OAControllerImpl
         dialogPage.setOkButtonItemName("DialogOk");
 
         dialogPage.setPostToCallingPage(true);
-        // am.invokeMethod("commitTransaction");
+        
+        am.invokeMethod("commitTransaction");
 
-        Serializable[] updateParams = { pItemKey, strProjectStatus };
+        Serializable[] updateParams = { pItemKey };
         am.invokeMethod("updateProjStatus", updateParams);
 
         
-        pageContext.redirectToDialogPage(dialogPage);
+        pageContext.redirectToDialogPage(dialogPage); 
         
       }
 
